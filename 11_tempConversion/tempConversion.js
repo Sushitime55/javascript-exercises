2db5a6350536adffcddf90e5ab3ee8c6452bde0c
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2025-09-13 21:41:28 -0700

    11-temp-conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // c = (f - 32)/1.8
+  newTemp = (temp - 32) / 1.8;
+  newTemp = Math.round(newTemp * 10) / 10;
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // f = 1.8c + 32
+  newTemp = temp * 1.8 + 32;
+  newTemp = Math.round(newTemp * 10) / 10;
+  return newTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
