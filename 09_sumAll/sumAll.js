4e29a50460cea60290fb19ce3bb506aa7b4ed2e1
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2025-09-13 21:23:48 -0700

    09-sum-all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0) return "ERROR";
 
+  if (!Number.isInteger(x) || !Number.isInteger(y)) {
+    return "ERROR";
+  }
+
+  let smaller = Math.min(x, y);
+  let larger = Math.max(x, y);
+  let sum = 0;
+  for (i = smaller; i <= larger; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
