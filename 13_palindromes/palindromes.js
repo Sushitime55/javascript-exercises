b6f776706918bb88ca708854161d636e05c2117a
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2025-09-17 00:26:30 -0700

    13-palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  // force lower case, remove punctuation
+  let cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => allowedChars.includes(char))
+    .join("");
+
+  // make 2nd clean string that is reversed
+  let reversedString = cleanedString.split("").reverse().join("");
+
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
