2d34c1ff964097a0ecd33eccf9188cd75ca8029c
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2025-09-17 00:43:31 -0700

    14-fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+// iterative
+const fibonacci = function (x) {
+  x = parseInt(x);
 
+  if (x < 0) {
+    return "OOPS";
+  }
+
+  if (x <= 1) {
+    return x;
+  }
+
+  let a = 0;
+  let b = 1;
+
+  while (x > 1) {
+    sum = a + b;
+    a = b;
+    b = sum;
+    x -= 1;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
