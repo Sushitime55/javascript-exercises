726e83f7b1a03f5c05b7620cda8f72833058d467
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2025-09-17 01:06:53 -0700

    16-find-the-oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+// const people = [
+//       {
+//         name: "Carly",
+//         yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//       },
 
+function getAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+
+  return death - birth;
+}
+
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+    // compare oldest age with current person's age
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
